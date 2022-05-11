bc217bec2d2e97531574e271b1e47f353841e8e7
GuilhermeBarroso <202010422@unifoa.edu.br>
2022-05-11 09:52:11 -0300

    feat: add favicon and remove remember-me

--- src/components/Authenticate/LoginForm/index.tsx
+++ src/components/Authenticate/LoginForm/index.tsx
@@ -59,7 +59,6 @@ export function LoginForm() {
 						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Entrar</h2>
 					</div>
 					<form  className="mt-8 space-y-6" onSubmit={handleSubmit} >
-						<input type="hidden" name="remember" defaultValue="true" />
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div className = {styles.errorEmail}><p>{invalidEmail && "Email Invalido"}</p></div>
 							<div>
@@ -98,19 +97,7 @@ export function LoginForm() {
 							</div>
 						</div>
 
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
-								<input
-									id="remember-me"
-									name="remember-me"
-									type="checkbox"
-									className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-								/>
-								<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-            Remember me
-								</label>
-							</div>
-						</div>
+				
 						{/* <div className="text-sm">
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
             Forgot your password?
--- src/components/Authenticate/RegisterForm/index.tsx
+++ src/components/Authenticate/RegisterForm/index.tsx
@@ -64,7 +64,7 @@ export function RegisterForm() {
 						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Registrar-se</h2>
 					</div>
 					<form  className="mt-8 space-y-6" onSubmit={handleSubmit}>
-						<input type="hidden" name="remember" defaultValue="true" />
+					
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div>
 								<label htmlFor="name-address" className="sr-only">nome</label>
@@ -121,19 +121,7 @@ export function RegisterForm() {
 							</div>
 						</div>
 					
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
-								<input
-									id="remember-me"
-									name="remember-me"
-									type="checkbox"
-									className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-								/>
-								<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-            Remember me
-								</label>
-							</div>
-						</div>
+
 						{/* <div className="text-sm">
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
             Forgot your password?
