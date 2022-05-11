d1b9ebc183b0d72b7399755247e616f32f787e87
GuilhermeBarroso <202010422@unifoa.edu.br>
2022-05-11 09:41:57 -0300

    fix: fixing placeholder

--- src/components/TodoList/CreateTodo/index.tsx
+++ src/components/TodoList/CreateTodo/index.tsx
@@ -40,7 +40,7 @@ export function CreateTodo({todolist,setTodolist} : CreateTodoProps) {
    
 			<form onSubmit={createTodo}>
 				<div className={`flex items-center justify-center transition-all duration-500 text-center`}>
-					<input ref={todoNameRef} maxLength={45} onChange={(event)=>{setTodoName(event.target.value);}} className="w-3/5 text-blue-500 font-extrabold placeholder-blue-400  py-1.5 px-2 border-blue-600 rounded border-2 focus:outline-none" type="text" placeholder="Digite o nome de sua tarefa aqui" aria-label="nome da tarefa" />
+					<input ref={todoNameRef}  onChange={(event)=>{setTodoName(event.target.value);}} className="w-3/5 text-blue-500 font-extrabold placeholder-blue-400  py-1.5 px-2 border-blue-600 rounded border-2 focus:outline-none" type="text" placeholder="Digite o nome" aria-label="nome da tarefa" />
 					<button className="flex-shrink-0 bg-blue-500 hover:bg-blue-600 border-blue-500 hover:border-blue-600 text-sm  text-white py-2 px-3 rounded" type="submit">
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
